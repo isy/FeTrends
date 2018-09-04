12cc22009e43277bf2b2ee24d83cee26709ef805
Hironori Akaishi <ah.gm3622@gmail.com>
2018-09-05 00:04:51 +0900

    add: axios

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,21 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential',
-    'eslint:recommended',
+    "plugin:vue/essential",
+    "eslint:recommended",
     "plugin:vue/recommended",
     "plugin:prettier/recommended"
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ["vue"],
   // add your custom rules here
-  rules: {}
-}
+  rules: {
+    semi: [2, "never"],
+    "prettier/prettier": ["error", { semi: false }]
+  }
+};
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,37 +1,40 @@
 module.exports = {
   head: {
-    title: 'FeTrends',
+    title: "FeTrends",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'トレンド商品' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "トレンド商品" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
-  css: ['@assets/style/app.css'],
-  loading: { color: '#3b80e8' },
+  css: ["@assets/style/app.css"],
+  loading: { color: "#3b80e8" },
   build: {
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/,
           options: {
-            fix: true,
-          },
-        })
+            fix: true
+          }
+        });
       }
     }
   },
   plugins: [
     {
-      src: '~/plugins/at-ui',
-      ssr: false,
-    },
+      // Doc: https://at-ui.github.io/at-ui/#/en
+      src: "~/plugins/at-ui",
+      ssr: false
+    }
   ],
-}
-
+  modules: [
+    // Doc: https://axios.nuxtjs.org/
+    "@nuxtjs/axios"
+  ],
+  axios: {}
+};
